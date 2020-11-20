c4659c3030d8e1711079f26059b0c650e9329fac
Elijahscriptdev <elijahobominuru@gmail.com>
2020-11-20 03:29:23 +0100

    remove our team section

--- src/components/aboutpage/AboutPage.js
+++ src/components/aboutpage/AboutPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, Col, Container, Jumbotron, Row } from "react-bootstrap";
+import { Col, Container, Jumbotron, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "./AboutPage.css";
 
