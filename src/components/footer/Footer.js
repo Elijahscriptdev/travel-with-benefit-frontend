8c3e71247588d78d8618eb077c0be24718b31be6
Elijahscriptdev <elijahobominuru@gmail.com>
2020-12-14 18:34:38 +0100

    git add styling to home page

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -8,7 +8,7 @@ function Footer() {
     <div className='footer-container'>
       <section className='footer-subscription text-white'>
         <p className='footer-subscription-heading'>
-          Join the Adventure newsletter to receive our best vacation deals
+          Join the Adventure newsletter to receive our best travel deals
         </p>
         <p className='footer-subscription-text'>
           You can unsubscribe at any time.
