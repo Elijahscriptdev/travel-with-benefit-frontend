24149cd6db53eae545460c502f5742b05204276d
Elijahscriptdev <elijahobominuru@gmail.com>
2020-12-16 08:22:46 +0100

    git add styling to home page

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -4,6 +4,7 @@ import axios from "axios";
 // import { connect } from "react-redux";
 // import { getProfile } from "../../redux/actions/profile";
 // import profile from "../../redux/reducers/profile";
+import "./Dashboard.css";
 
 const Dashboard = () => {
   const [booking, setBooking] = useState([]);
@@ -41,8 +42,8 @@ const Dashboard = () => {
   };
 
   return (
-    <div>
-      <h1>Dashboard</h1>
+    <div className='dash-info p-5'>
+      <h1 className='text-right'>Welcome {data.first_name}!!!</h1>
       <form className='form' onSubmit={(e) => onSubmit(e)}>
         <div className='form-group'>
           <input
@@ -62,9 +63,94 @@ const Dashboard = () => {
       </form>
 
       <div>
-        {booking.map((booking, index) => (
-          <p key={index}>{booking.email}</p>
-        ))}
+        <table className='table'>
+          <thead>
+            <tr>
+              {/* <th scope='col'>Email</th> */}
+              <th scope='col' colSpan={2}>
+                First Name
+              </th>
+              <th scope='col' colSpan={2}>
+                Last Name
+              </th>
+              <th scope='col' colSpan={2}>
+                Booking Ref
+              </th>
+              <th scope='col' colSpan={2}>
+                Departure
+              </th>
+              <th scope='col' colSpan={2}>
+                Destination
+              </th>
+              <th scope='col' colSpan={2}>
+                Bus
+              </th>
+              <th scope='col' colSpan={2}>
+                Company
+              </th>
+              <th scope='col' colSpan={2}>
+                Traval Date
+              </th>
+              <th scope='col' colSpan={2}>
+                Travel Time
+              </th>
+              <th scope='col' colSpan={2}>
+                Price
+              </th>
+              <th scope='col' colSpan={2}>
+                Seats
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {booking.length > 0 ? (
+              booking.map(
+                booking(
+                  <tr key={booking.id}>
+                    {/* <td data-label='email'>{booking.email}</td> */}
+                    <td data-label='first name' colSpan={2}>
+                      {booking.first_name}
+                    </td>
+                    <td data-label='last name' colSpan={2}>
+                      {booking.last_name}
+                    </td>
+                    <td data-label='booking ref' colSpan={2}>
+                      {booking.booking_ref}
+                    </td>
+                    <td data-label='departure' colSpan={2}>
+                      {booking.departure}
+                    </td>
+                    <td data-label='destination' colSpan={2}>
+                      {booking.destination}
+                    </td>
+                    <td data-label='bus_type' colSpan={2}>
+                      {booking.bus_type}
+                    </td>
+                    <td data-label='bus company' colSpan={2}>
+                      {booking.bus_company}
+                    </td>
+                    <td data-label='travel date' colSpan={2}>
+                      {booking.travel_date}
+                    </td>
+                    <td data-label='travel time' colSpan={2}>
+                      {booking.travel_time}
+                    </td>
+                    <td data-label='price' colSpan={2}>
+                      {booking.total_price}
+                    </td>
+                    <td data-label='seat' colSpan={2}>
+                      {booking.seats}
+                    </td>
+                  </tr>
+                )
+              )
+            ) : (
+              <tr>
+                <td colSpan={2}>No Booking</td>
+              </tr>
+            )}
+          </tbody>
+        </table>
       </div>
     </div>
   );
